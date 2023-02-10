bc12c2b409e7f8ff921281926638a76e71c5b3d4
Karthiga1700 <100156856+Karthiga1700@users.noreply.github.com>
2023-02-10 17:03:27 -0600

    Update variables.tf

--- Bucket/variables.tf
+++ Bucket/variables.tf
@@ -2,6 +2,6 @@ variable "tenancy_ocid"{}
 variable "region"{}
 variable "compartment_ocid"{}
 variable "bucket_name"{
-    default = "Test-Bucket"
+    default = "Bucket"
 }
 variable "bucket_namespace"{}
