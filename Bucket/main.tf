872115c72c1ce3da939e03f50e696122384e37a1
Karthiga1700 <100156856+Karthiga1700@users.noreply.github.com>
2023-02-10 17:52:46 -0600

    Update main.tf

--- Bucket/main.tf
+++ Bucket/main.tf
@@ -7,5 +7,11 @@ resource "oci_objectstorage_bucket" "test_bucket" {
     #Required
     compartment_id = var.compartment_ocid
     name = var.bucket_name
-    namespace = data.oci_objectstorage_namespace.test_bucket.namespace
+    namespace = data.oci_objectstorage_namespace.test_namespace.namespace
+}
+
+data "oci_objectstorage_namespace" "test_namespace" {
+
+    #Optional
+    compartment_id = var.compartment_ocid
 }
