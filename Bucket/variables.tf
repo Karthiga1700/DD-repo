1fe77cdf4c4e4650e5f83d07c0fd288c03122adb
Karthiga1700 <100156856+Karthiga1700@users.noreply.github.com>
2023-02-13 23:21:42 -0600

    Delete Bucket directory

--- Bucket/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "tenancy_ocid"{}
-variable "region"{}
-variable "compartment_ocid"{}
-variable "bucket_name"{
-    default = "Bucket"
-}
-variable "bucket_namespace"{
-     default = "Bucket"
-}
